66e2dbfde525b6b352d1c76976174690950ff559
Dungkool <124110209+Dungkool@users.noreply.github.com>
2023-02-23 17:03:58 +0900

    Update P68.java
    update

--- day02/variable/P68.java
+++ day02/variable/P68.java
@@ -11,10 +11,6 @@ public class P68 {
 		int a = 10;
 		System.out.println(s2 + a); // String과 숫자를 더하면 숫자가 문자로 바뀜
 
-		int n1 = 10;
-		String n2 = String.valueOf(n1); // 숫자를 문자로 변환
-		// String n2 = n1+""; 문자와 숫자를 더해서 문자로 변환
-
 		Random r = new Random(); // jdk 쓸 떄 밑에다가 패키지 import해야 함, Ctrl + Shift + O
 		int rndNum = r.nextInt(45)+1;
 		System.out.println(rndNum);

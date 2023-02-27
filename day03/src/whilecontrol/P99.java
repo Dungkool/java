559ad32c23baedc224ad9b66ba219b9f6e7f3cb0
jhs4132@gmail.com <jhs4132@gmail.com>
2023-02-27 09:24:25 +0900

    test_P99

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,6 +4,8 @@ public class P99 {
 
 	public static void main(String[] args) {
 		for(int i = 2; i < 10; i++) {
+			
+			int bb = 200;
 			System.out.printf("%d 단 \n",i);
 			for(int j = 1; j < 10; j++) {
 				System.out.printf("%d * %d = %d \n", i, j, (i * j));
